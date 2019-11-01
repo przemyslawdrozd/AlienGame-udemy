cf3f02718186d863b2d82880ca3184936ec1d397
przemo06133 <przemo06133@gmail.com>
2019-11-01 12:10:47 +0100

    Constants for game

---
+++ src/com/company/constants/Constants.java
@@ -0,0 +1,25 @@
+package com.company.constants;
+
+import java.util.Collections;
+
+public class Constants {
+
+    private Constants(){
+    }
+
+    public static final String TITLE = "Space Invasion";
+    public static final int BOARD_WIDTH = 900;
+    public static final int BOARD_HEIGHT = 750;
+
+    // speed of the application
+    public static final int GAME_SPEED = 10;
+    public static final int SPACESHIP_WIDTH = 34;
+    public static final int SPACESHIP_HEIGHT = 28;
+
+    // images for the objects
+    public static final String UFO_IMAGE_URL = "/Users/przemo06133/Documents/aliengame/src/images/ufo.png";
+    public static final String LASER_IMAGE_URL = "/Users/przemo06133/Documents/aliengame/src/images/laser.png";
+    public static final String BOMB_IMAGE_URL = "/Users/przemo06133/Documents/aliengame/src/images/bomb.png";
+    public static final String BACKGROUND_IMAGE_URL = "/Users/przemo06133/Documents/aliengame/src/images/background.jpg";
+    public static final String SPACESHIP_IMAGE_URL = "/Users/przemo06133/Documents/aliengame/src/images/spaceship.png";
+}
